34bb48460294ef72bff8e6dbe36712507c29a01e
thiagosalome <thiagosalome@gmail.com>
2018-05-29 21:22:59 -0300

    Subindo classe de entidade Filme

---
+++ php/entities/Filme.php
@@ -0,0 +1,138 @@
+<?php
+
+/**
+ * @Entity
+ * @Table(name="filme")
+ */
+class Filme{
+    /**
+     * @Id
+     * @GeneratedValue(strategy="AUTO")
+     * @Column(type="integer", name="id")
+     */
+    protected $id;
+
+    /**
+     * @Column(type="string", name="titulo")
+     */
+    protected $title;
+
+    /**
+     * @Column(type="float", name="verba")
+     */
+    protected $verba;
+
+    /**
+     * @Column(type="date", name="dtGravacaoInicio")
+     */
+    protected $dtGravacaoInicio;
+
+    /**
+     * @Column(type="date", name="dtGravacaoFim")
+     */
+    protected $dtGravacaoFim;
+
+
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */ 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of verba
+     */ 
+    public function getVerba()
+    {
+        return $this->verba;
+    }
+
+    /**
+     * Set the value of verba
+     *
+     * @return  self
+     */ 
+    public function setVerba($verba)
+    {
+        $this->verba = $verba;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dtGravacaoInicio
+     */ 
+    public function getDtGravacaoInicio()
+    {
+        return $this->dtGravacaoInicio;
+    }
+
+    /**
+     * Set the value of dtGravacaoInicio
+     *
+     * @return  self
+     */ 
+    public function setDtGravacaoInicio($dtGravacaoInicio)
+    {
+        $this->dtGravacaoInicio = $dtGravacaoInicio;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dtGravacaoFim
+     */ 
+    public function getDtGravacaoFim()
+    {
+        return $this->dtGravacaoFim;
+    }
+
+    /**
+     * Set the value of dtGravacaoFim
+     *
+     * @return  self
+     */ 
+    public function setDtGravacaoFim($dtGravacaoFim)
+    {
+        $this->dtGravacaoFim = $dtGravacaoFim;
+
+        return $this;
+    }
+}
+
+?>
